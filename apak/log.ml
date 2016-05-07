7784ca7273f7e345da96b981f8fd15b7646bdc52
Zachary Kincaid <zkincaid@gmail.com>
2016-05-07 18:09:51 -0400

    Pull in apak.log from ark2 branch to fix segfaults

--- apak/log.ml
+++ apak/log.ml
@@ -12,9 +12,7 @@ let debug_mode      = ref false
 let default_debug   = ref false
 let debug_phases    = ref ([] : string list)
 let verbosity_level : level ref = ref `always
-
-let terminal_supports_colors =
-  Unix.isatty Unix.stdout
+let colorize        = ref (Unix.isatty Unix.stdout)
 
 let level_of_string = function
   | "trace" -> `trace
@@ -26,19 +24,6 @@ let level_of_string = function
   | "always" -> `always
   | lev ->
     Format.ksprintf invalid_arg "Unrecognized level: `%s'" lev
-
-let attr_code = function
-  | `Black -> "\x1b[30m"
-  | `Red -> "\x1b[31m"
-  | `Green -> "\x1b[32m"
-  | `Yellow -> "\x1b[33m"
-  | `Blue -> "\x1b[34m"
-  | `Magenta -> "\x1b[35m"
-  | `Cyan -> "\x1b[36m"
-  | `White -> "\x1b[37m"
-  | `Bold -> "\x1b[1m"
-  | `Underline -> "\x1b[4m"
-let reset_attributes = "\x1b[0m"
 
 let loggers = Hashtbl.create 32
 let set_verbosity_level name (value : level) =
@@ -54,24 +39,43 @@ module Make(M : sig val name : string end) = struct
     Hashtbl.add loggers M.name my_verbosity_level
 
   let logf ?(level=`info) ?(attributes=[]) fmt =
+    let attr_code = function
+      | `Black -> format_of_string "\x1b[30m"
+      | `Red -> "\x1b[31m"
+      | `Green -> "\x1b[32m"
+      | `Yellow -> "\x1b[33m"
+      | `Blue -> "\x1b[34m"
+      | `Magenta -> "\x1b[35m"
+      | `Cyan -> "\x1b[36m"
+      | `White -> "\x1b[37m"
+      | `Bold -> "\x1b[1m"
+      | `Underline -> "\x1b[4m"
+    in
+    let format_of_attrs =
+      List.fold_left
+        (fun fmt attr -> fmt ^^ (format_of_string (attr_code attr)))
+        ""
+    in
     let (start, reset) =
-      Obj.magic
-        (if terminal_supports_colors && attributes != []
-         then (String.concat "" (List.map attr_code attributes), "\x1b[0m@\n@?")
-         else ("", "@\n@?"))
+      if !colorize && attributes != [] then
+        (format_of_attrs attributes, format_of_string "\x1b[0m@\n@?")
+      else
+        ("", "@\n@?")
     in
     if level_leq (!verbosity_level) level
        || level_leq (!my_verbosity_level) level
     then
-      Format.printf (start ^^ fmt ^^ reset)
+      Format.fprintf Format.std_formatter (start ^^ fmt ^^ reset)
     else
       Format.ifprintf Format.std_formatter fmt
 
+
   let log ?(level=`info) ?(attributes=[]) str =
-    logf ~level:level ~attributes:attributes "%s" str
+    logf ~level ~attributes "%s" str
 
   let log_pp ?(level=`info) ?(attributes=[]) pp x =
-    logf ~level:level ~attributes:attributes "%a" pp x
+    logf ~level ~attributes "%a" pp x
+
 end
 
 include Make(struct let name = "default" end)
@@ -102,13 +106,20 @@ let debugf fmt =
   else Format.ifprintf Format.std_formatter fmt
 
 let errorf fmt =
-  if terminal_supports_colors
+  if !colorize
   then Format.eprintf ("\x1b[31;1m" ^^ fmt ^^ "\x1b[0m@\n@?")
   else Format.eprintf fmt
 let error msg = errorf msg
 let error_pp pp x = errorf "%a" pp x
 
-let fatalf fmt = Format.kfprintf (fun _ -> exit (-1)) fmt
+let fatalf fmt =
+  let fmt =
+    if !colorize
+    then ("\x1b[31;1m" ^^ fmt ^^ "\x1b[0m@\n@?")
+    else fmt
+  in
+  Format.kfprintf (fun _ -> exit (-1)) Format.err_formatter fmt
+
 let fatal msg = fatalf msg
 
 let invalid_argf fmt = Format.ksprintf invalid_arg fmt
@@ -143,7 +154,8 @@ let phase str f arg =
 
 let start_time = Unix.gettimeofday ()
 let print_stats () =
-  let f stat time = print_endline (stat ^ ": " ^ (string_of_float time)) in
+  let f stat time = print_endline (stat ^ ": " ^ (string_of_float time))
+  in
   Hashtbl.iter f phases_ht;
   print_endline ("Total time: "
                  ^ (string_of_float (Unix.gettimeofday () -. start_time)))
