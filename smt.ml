7366a15ceda42ef2280c9641201d2dd5c477e990
Zachary Kincaid <zkincaid@gmail.com>
2017-04-06 19:22:14 -0400

    Extend interpretations to functions

--- smt.ml
+++ smt.ml
@@ -2,26 +2,6 @@ open Syntax
 open BatPervasives
 
 include Apak.Log.Make(struct let name = "ark.smt" end)
-
-class type ['a] smt_model = object
-  method eval_int : 'a term -> ZZ.t
-  method eval_real : 'a term -> QQ.t
-  method sat :  'a formula -> bool
-  method to_string : unit -> string
-end
-
-class type ['a] smt_solver = object
-  method add : ('a formula) list -> unit
-  method push : unit -> unit
-  method pop : int -> unit
-  method reset : unit -> unit
-  method check : ('a formula) list -> [ `Sat | `Unsat | `Unknown ]  
-  method to_string : unit -> string
-  method get_model : unit -> [ `Sat of 'a smt_model | `Unsat | `Unknown ]
-  method get_unsat_core : ('a formula) list ->
-    [ `Sat | `Unsat of ('a formula) list | `Unknown ]
-end
-
 let default_solver = ref `Z3
 
 let mk_solver ark =
