d8f36b2aaa0a0ae485932e3b87e4689c85fe8f9b
Zak Kincaid <zkincaid@gmail.com>
2018-05-10 12:31:08 -0400

    Removed unused vars

--- src/srkZ3.ml
+++ src/srkZ3.ml
@@ -611,9 +611,7 @@ let simplify ?(context=Z3.mk_context []) srk phi =
   let open Z3 in
   let open Tactic in
   let z3 = context in
-  let f t t' = Tactic.and_then z3 t t' [] in
   let mk_tactic = mk_tactic z3 in
-  let sym = Symbol.mk_string z3 in
   let solve_eqs =
     when_ z3
       (Probe.not_ z3 (Probe.mk_probe z3 "has-patterns"))
@@ -630,7 +628,6 @@ let simplify ?(context=Z3.mk_context []) srk phi =
   in
   let g = Goal.mk_goal z3 false false false in
   Goal.add g [z3_of_formula srk z3 phi];
-  let g' = Tactic.apply simplify g None in
   try
     of_apply_result srk (Tactic.apply simplify g None)
   with _ -> assert false
