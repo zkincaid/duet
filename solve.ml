792672c2430a3268f9eeef40e3292bae7a0f48d6
Jason Breck <jbreck@cs.wisc.edu>
2018-05-23 11:02:51 -0500

    Improved scanOverSCC in Karp

--- solve.ml
+++ solve.ml
@@ -182,19 +182,16 @@ let karpBestCycleMean graph nSCCs mapVertexToSCC mapSCCToVertices =
               | Fin fnv -> 
                 (* The worst, over all numbers of steps (steps) ...         *)
                 (*                                                          *)
-                (* First, we scan over all numbers of steps;                *)
-                (*   we will filter out infinite F_k(v) values.             *)
-                let rec scanOverSteps steps pairs = 
-                    if (steps >= nVertices) then pairs else
+                (* First, we scan over all numbers of steps k;              *)
+                (*   we filter out infinite F_k(v) values.                  *)
+                let pairs = loopFromMToN 0 (nVertices - 1) [] (fun steps pairs ->
                     (* Karp's F_k(v) *)
                     let fkv = (IntIntMap.find (nVertices, vVertex) fMap) in
-                    let pairs = match fkv with 
-                                (* Ignore the F_k(v) for this k if          *)
-                                (*    this F_k(v) is infinite.              *)
-                                | Worst -> pairs
-                                | Fin fin_fkv -> (steps, fin_fkv) :: pairs in
-                    scanOverSteps (steps + 1) pairs
-                in let pairs = (scanOverSteps 0 []) in
+                    match fkv with 
+                    (* Ignore the F_k(v) for this k if this F_k(v) is       *)
+                    (*    infinite.                                         *)
+                    | Worst -> pairs
+                    | Fin fin_fkv -> (steps, fin_fkv) :: pairs) in
                 (* Now scan over pairs (steps, fkv) having finite fkv       *)
                 match pairs with 
                 | [] -> failwith "Failure in Karp's algorithm"
