11d23f4a9c4ae13656439c452a53f1dafb13e5c8
Zak Kincaid <zkincaid@gmail.com>
2019-08-14 19:58:42 -0400

    Sign analysis and new forward invariant generation interface

--- test/test_WeightedGraph.ml
+++ test/test_WeightedGraph.ml
@@ -201,6 +201,8 @@ let recursive () =
   assert_post (RG.path_weight query 0 2) (x + y = (int 100));
   assert_not_post (RG.path_weight query 0 2) (y <= (int 99))
 
+let affine_invariants = TS.forward_invariants (module TS.AffineRelation)
+
 let aff_eq1 () =
   let open Infix in
   let ts =
@@ -211,7 +213,7 @@ let aff_eq1 () =
        (3, T.assign "y" (y + (int 1)), 1)]
       []
   in
-  let inv = TS.affine_invariants ts 0 in
+  let inv = affine_invariants ts 0 in
   assert_equiv_formula (y - x = (int 10)) (SrkApron.formula_of_property (inv 1));
   assert_equiv_formula (y - x = (int 9)) (SrkApron.formula_of_property (inv 3))
 
@@ -236,7 +238,7 @@ let aff_collatz () =
         1)]
       []
   in
-  let inv = TS.affine_invariants ts 0 in
+  let inv = affine_invariants ts 0 in
   assert_equiv_formula (x = y) (SrkApron.formula_of_property (inv 1))
 
 let aff_karr_fig4 () =
@@ -251,7 +253,7 @@ let aff_karr_fig4 () =
       ]
       []
   in
-  let inv = TS.affine_invariants ts 0 in
+  let inv = affine_invariants ts 0 in
   assert_equiv_formula (x = y + (int 1)) (SrkApron.formula_of_property (inv 1));
   assert_equiv_formula (x = y - (int 1)) (SrkApron.formula_of_property (inv 2))
 
@@ -271,7 +273,7 @@ let aff_karr_fig5 () =
         1)]
       []
   in
-  let inv = TS.affine_invariants ts 0 in
+  let inv = affine_invariants ts 0 in
   assert_equiv_formula ((int 3)*x - y + z = (int 1)) (SrkApron.formula_of_property (inv 1))
 
 
