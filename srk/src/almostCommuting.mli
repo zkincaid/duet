3b2a401a82f9583940de014151ace574511602a4
Andreas Humenberger <ahumenberger@gmail.com>
2020-01-16 08:47:16 +0100

    Docs

--- srk/src/almostCommuting.mli
+++ srk/src/almostCommuting.mli
@@ -10,10 +10,10 @@ type phased_segment = {
    sim2 : QQMatrix.t;                 (* Simulation for phase 2 (all non-reset matrices commute) *)
    phase1 : QQMatrix.t array;         (* Image of each transition under the phase 1 simulation *)
    phase2 : (kind * QQMatrix.t) array (* Each transition i is either of kind Commute, in which case
-                                         phase2.(i) is the image of transition i under the phase 1
+                                         phase2.(i) is the image of transition i under the phase 2
                                          simulation, or of kind Reset, in which case phase2.(i) gives
-                                         a representation of transition i as a transformation from the phase 1
-                                         space to the phase 2 space *)
+                                         a representation of transition i as a transformation from the 
+                                         phase 1 space to the phase 2 space *)
 }
 
 module PhasedSegment : sig
