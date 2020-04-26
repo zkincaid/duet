4dab134fc78b44dc42b06a6d43254d1c6660bea5
Zachary Kincaid <zkincaid@gmail.com>
2020-04-26 10:13:19 -0400

    Omega regular algebras

--- srk/src/pathexpr.ml
+++ srk/src/pathexpr.ml
@@ -17,7 +17,21 @@ type pe =
   | Star of t
   | One
   | Zero
+  | Omega of t
 and t = pe hobj
+
+(* Path expressions & omega path expressions are represented by the
+   same type.  The type equality is not exported, and well-formedness
+   of (omega) path expressions is enforced by the structure of the
+   module. *)
+type omega = t
+
+type ('a,'b) open_omega_pathexpr =
+  [ `Omega of 'a
+  | `Mul of 'a * 'b
+  | `Add of 'b * 'b ]
+
+type ('a,'b) omega_algebra = ('a,'b) open_omega_pathexpr -> 'b
 
 module HC = BatHashcons.MakeTable(struct
     type t = pe
@@ -27,14 +41,16 @@ module HC = BatHashcons.MakeTable(struct
       | Mul (s, t), Mul (s', t') -> s.tag = s'.tag && t.tag = t'.tag
       | Add (s, t), Add (s', t') -> s.tag = s'.tag && t.tag = t'.tag
       | Star t, Star t' -> t.tag = t'.tag
+      | Omega t, Omega t' -> t.tag = t'.tag
       | _ -> false
     let hash = function
       | Edge (x, y) -> Hashtbl.hash (0, x, y)
       | Mul (x, y) -> Hashtbl.hash (1, x.tag, y.tag)
       | Add (x, y) -> Hashtbl.hash (2, x.tag, y.tag)
       | Star x -> Hashtbl.hash (3, x.tag)
-      | Zero -> Hashtbl.hash 4
-      | One -> Hashtbl.hash 5
+      | Omega x -> Hashtbl.hash (4, x.tag)
+      | Zero -> Hashtbl.hash 5
+      | One -> Hashtbl.hash 6
   end)
 module HT = BatHashtbl.Make(struct
     type t = pe hobj
@@ -44,6 +60,7 @@ module HT = BatHashtbl.Make(struct
 
 type context = HC.t
 type 'a table = 'a HT.t
+type ('a,'b) omega_table = ('a table * 'b HT.t)
 
 let mk_one pe = HC.hashcons pe One
 let mk_zero pe = HC.hashcons pe Zero
@@ -60,6 +77,10 @@ let mk_add pe x y = match x.obj, y.obj with
 let mk_star pe x = HC.hashcons pe (Star x)
 let mk_edge pe src tgt = HC.hashcons pe (Edge (src, tgt))
 
+let mk_omega pe x = HC.hashcons pe (Omega x)
+let mk_omega_add = mk_add
+let mk_omega_mul = mk_mul
+
 let rec pp formatter pathexpr =
   let open Format in
   match pathexpr.obj with
@@ -67,11 +88,15 @@ let rec pp formatter pathexpr =
   | Mul (x, y) -> fprintf formatter "@[(%a)@ . (%a)@]" pp x pp y
   | Add (x, y) -> fprintf formatter "@[%a@ + %a@]" pp x pp y
   | Star x -> fprintf formatter "@[(%a)*@]" pp x
+  | Omega x -> fprintf formatter "@[(%a)w@]" pp x
   | Zero -> fprintf formatter "0"
   | One -> fprintf formatter "1"
 
-let mk_table ?(size=991) () = HT.create size      
+let pp_omega = pp
+
+let mk_table ?(size=991) () = HT.create size
 let mk_context ?(size=991) () = HC.create size
+let mk_omega_table ?(size=991) table = (table, HT.create size)
 
 let eval ?(table=HT.create 991) (f : 'a open_pathexpr -> 'a) =
   let rec go expr =
@@ -86,8 +111,30 @@ let eval ?(table=HT.create 991) (f : 'a open_pathexpr -> 'a) =
         | Add (x, y) -> f (`Add (go x, go y))
         | Star x -> f (`Star (go x))
         | Edge (s, t) -> f (`Edge (s, t))
+        | Omega _ -> assert false
       in
       HT.add table expr result;
+      result
+  in
+  go
+
+let eval_omega
+      ?(table=(HT.create 991,HT.create 991))
+      (f : 'a algebra)
+      (g : ('a,'b) omega_algebra) =
+  let (table, omega_table) = table in
+  let rec go expr =
+    if HT.mem omega_table expr then
+      HT.find omega_table expr
+    else
+      let result =
+        match expr.obj with
+        | Omega x -> g (`Omega (eval ~table f x))
+        | Add (x, y) -> g (`Add (go x, go y))
+        | Mul (x, y) -> g (`Mul (eval ~table f x, go y))
+        | _ -> assert false
+      in
+      HT.add omega_table expr result;
       result
   in
   go
--- srk/src/pathexpr.mli
+++ srk/src/pathexpr.mli
@@ -1,4 +1,14 @@
+(** Path expressions *)
+
+(** A path expression for a graph [G] is a regular expression over the
+   alphabet of the edges of [G], in which every recognized word is a
+   finite path in [G] *)
 type t
+
+(** An omega path expression for a graph [G] is an omega regular
+   expression over the alphabet of the edges of [G], in which every
+   recognized omega word is an infinite path in [G] *)
+type omega
 
 type 'a open_pathexpr =
   [ `Edge of int * int
@@ -8,17 +18,30 @@ type 'a open_pathexpr =
   | `Zero
   | `One ]
 
+type ('a,'b) open_omega_pathexpr =
+  [ `Omega of 'a
+  | `Mul of 'a * 'b
+  | `Add of 'b * 'b ]
+  
 (** An algebra is a structure equiped with operations for interpreting each
     path expression operation.  *)
 type 'a algebra = 'a open_pathexpr -> 'a
 
+(** An omega algebra is a structure equiped with operations for
+   interpreting each omega path expression operation.  *)
+type ('a,'b) omega_algebra = ('a,'b) open_omega_pathexpr -> 'b
+
 (** Memo table for weight homomorphisms *)
 type 'a table
+
+(** Memo table for omega weight homomorphisms *)
+type ('a,'b) omega_table
 
 (** Context for managing path expressions *)
 type context
 
 val pp : Format.formatter -> t -> unit
+val pp_omega : Format.formatter -> omega -> unit
 
 (** Create a new path expression context, with a given (optional) table size
     for hashconsed path expressions. *)
@@ -31,15 +54,33 @@ val mk_one : context -> t
 val mk_star : context -> t -> t
 val mk_edge : context -> int -> int -> t
 
+val mk_omega : context -> t -> omega
+val mk_omega_add : context -> omega -> omega -> omega
+val mk_omega_mul : context -> t -> omega -> omega
+
 (** Evaluate a path expression within some other algebra.  Recursive calls
     to [eval] are memoized so that repeated sub-path expressions are
     evaluated only once.  The optional [table] parameter allows calls to be
     memoized over multiple top-level calls. *)
-val eval : ?table:('a table) -> ('a open_pathexpr -> 'a) -> t -> 'a
+val eval : ?table:('a table) -> 'a algebra -> t -> 'a
 
 (** Create a new memo table *)
 val mk_table : ?size:int -> unit -> 'a table
 
+(** Create a new omega memo table *)
+val mk_omega_table : ?size:int -> 'a table -> ('a,'b) omega_table
+
 (** Forget memoized values for path expressions that involve edges that do
     not satisfy the given predicate. *)
 val forget : 'a table -> (int -> int -> bool) -> unit
+
+(** Evaluate an omega path expression within some other algebra.
+   Recursive calls to [eval_omega] are memoized so that repeated
+   sub-path expressions are evaluated only once.  The optional [table]
+   parameter allows calls to be memoized over multiple top-level
+   calls. *)
+val eval_omega : ?table:(('a,'b) omega_table) ->
+                 'a algebra ->
+                 ('a,'b) omega_algebra ->
+                 omega ->
+                 'b
