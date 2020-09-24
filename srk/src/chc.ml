c3b1fc44cd69ea24c8fa871237099c39cc1aa7e1
JakeSilverman <JakeRossSilverman@gmail.com>
2020-09-24 04:41:53 -0400

    work towards a chc api

--- srk/src/chc.ml
+++ srk/src/chc.ml
@@ -1,28 +1,276 @@
 (* Constrainted horn clauses *)
 open Syntax
+module DynArray = BatDynArray
 
-type 'a chccontext = 
-  { srk : 'a context;  relations : (string * typ list) BatDynArray.t}
-type relation = string [@@deriving ord]
-type relation_atom = relation * symbol list
-type 'a hypothesis = relation_atom list * 'a formula
-type conclusion = relation_atom
-type 'a rule = 'a hypothesis * conclusion
-type 'a query = relation
-type 'a chc = {rules : 'a rule list; queries : 'a query list}
+type relation = int
+type 'b relcontext = (string * typ list) DynArray.t
+type 'b relation_atom = relation * symbol list
+type ('a, 'b) hypothesis = 'b relation_atom list * 'a formula
+type ('a, 'b) rule = ('a, 'b) hypothesis * 'b relation_atom
+type ('a, 'b) query = relation
+type 'c rule_name = int
+type 'c query_name = int
+type ('a, 'b, 'c) chc = 
+  {rules : ('c rule_name, ('a, 'b) rule)  Hashtbl.t;
+   rule_counter : int ref;
+   queries : ('c query_name, ('a, 'b) query)  Hashtbl.t;
+   query_counter : int ref}
+
+let mk_relcontext = DynArray.create ()
 
 module Relation = struct
-  type t = relation [@@deriving ord]
+  type t = relation 
+  module Set = SrkUtil.Int.Set 
+  let compare = Stdlib.compare
+  let mk_relation rel_ctx ?(name="R") typ =
+      DynArray.add rel_ctx (name, typ);
+      DynArray.length rel_ctx - 1
+
+  let type_of rel_ctx rel = snd (DynArray.get rel_ctx rel)
+  let name_of rel_ctx rel = fst (DynArray.get rel_ctx rel)
+  let pp rel_ctx formatter rel =
+    Format.fprintf formatter "@%s:R%n@" (name_of rel_ctx rel) rel
 end
-
-module RSet = BatSet.Make(Relation)
-
 
 let rel_sym_of (relation, _) = relation
 let params_of (_, params) = params
 
+let pp_relation_atom srk rel_ctx formatter (rel, symbols) =
+    Format.fprintf formatter "%a(@[%a@])"
+      (Relation.pp rel_ctx) rel
+      (SrkUtil.pp_print_enum_nobox (pp_symbol srk)) (BatList.enum symbols)
+let pp_hypothesis srk rel_ctx formatter (rel_atoms, phi) =
+  Format.fprintf formatter "(@[";
+  SrkUtil.pp_print_enum
+    ~pp_sep:(fun formatter () -> Format.fprintf formatter "@ /\\ ")
+    (pp_relation_atom srk rel_ctx)
+    formatter
+    (BatList.enum rel_atoms);
+  Format.fprintf formatter "@]/\\%a)" (Formula.pp srk) phi
+let pp_rule srk rel_ctx formatter (hypo, conc) =
+  Format.fprintf formatter "(@[%a@ => %a@])"
+    (pp_hypothesis srk rel_ctx) hypo
+    (pp_relation_atom srk rel_ctx) conc
+let pp_query _ = Relation.pp
 
 
+
+module Chc = struct
+  type ('a, 'b, 'c) t = ('a, 'b, 'c) chc 
+
+  let pp = failwith "TODO" 
+
+  let create ?(rlength=97) ?(qlength=97) () =
+    let rules = Hashtbl.create rlength in
+    let queries = Hashtbl.create qlength in
+    let rule_counter = ref 0 in
+    let query_counter = ref 0 in
+    {rules;rule_counter;queries;query_counter}
+
+  let copy chc = 
+    {rules=Hashtbl.copy chc.rules;
+     rule_counter = ref !(chc.rule_counter);
+     queries=Hashtbl.copy chc.queries;
+     query_counter = ref !(chc.query_counter)}
+
+  let add tbl counter obj =
+    Hashtbl.add tbl !counter obj;
+    counter := !(counter) + 1;
+    !(counter) - 1
+
+  let add_rule chc rule = add chc.rules chc.rule_counter rule
+  let add_query chc query = add chc.queries chc.query_counter query
+
+
+  let del tbl name =
+    if Hashtbl.mem tbl name then (
+      BatHashtbl.remove_all tbl name;
+      true)
+    else false
+
+  let del_rule chc rule_name = del chc.rules rule_name
+  let del_query chc query_name = del chc.queries query_name
+
+  let upd tbl name obj =
+    Hashtbl.add tbl name obj
+
+  let upd_rule chc rule_name rule = upd chc.rules rule_name rule
+  let upd_query chc query_name query = upd chc.queries query_name query
+
+  let get_rules chc = chc.rules
+  let get_queries chc = chc.queries
+  let get_rule chc rule_name = Hashtbl.find chc.rules rule_name
+  let get_query chc query_name = Hashtbl.find chc.queries query_name
+
+  let get_relations_used chc =
+    Hashtbl.fold
+      (fun _ ((hypo_atoms, _), conc_atom) rset ->
+         List.fold_left
+           (fun rset rel_atom -> Relation.Set.add (rel_sym_of rel_atom) rset)
+           (Relation.Set.add (rel_sym_of conc_atom) rset)
+           hypo_atoms)
+      chc.rules
+      (Hashtbl.fold 
+         (fun _ query rset -> Relation.Set.add query rset)
+         chc.queries
+         Relation.Set.empty)
+
+  let is_linear chc =
+    BatHashtbl.fold 
+      (fun _ ((rel_atoms, _), _) acc -> acc && (List.length rel_atoms) <= 1)
+      chc.rules
+      true
+
+end
+
+module ChcSrkZ3 = struct
+  
+  let typ_of_sort sort =
+    let open Z3enums in
+    match Z3.Sort.get_sort_kind sort with
+    | REAL_SORT -> `TyReal
+    | INT_SORT -> `TyInt
+    | BOOL_SORT -> `TyBool
+    | _ -> failwith "TODO: allow function types"
+
+  let rel_atom_of_z3 srk rel_ctx ind_to_sym rsym_to_int names z3pred =
+    let args = List.map 
+        (fun arg -> 
+           let index = Z3.Quantifier.get_index arg in
+           if BatHashtbl.mem ind_to_sym index then
+             BatHashtbl.find ind_to_sym index
+           else (
+             let sort = typ_of_sort (Z3.Expr.get_sort arg) in
+             let sym = 
+               mk_symbol srk ~name:(Z3.Symbol.to_string names.(index)) sort 
+             in
+             BatHashtbl.add ind_to_sym index sym;
+             sym)) 
+        (Z3.Expr.get_args z3pred) in
+    let decl = Z3.Expr.get_func_decl z3pred in
+    let rsym = Z3.Symbol.to_string (Z3.FuncDecl.get_name decl) in
+    let relation = 
+      if Hashtbl.mem rsym_to_int rsym then Hashtbl.find rsym_to_int rsym
+      else (
+        let typ = List.map (fun arg -> typ_symbol srk arg) args in
+        let res = Relation.mk_relation rel_ctx ~name:rsym typ in
+        Hashtbl.add rsym_to_int rsym res;
+        res)
+    in
+    relation, args
+
+  let rel_atom_of_z3_fresh srk rel_ctx ind_to_sym rsym_to_int names z3pred =
+    let fresh_index_map = BatHashtbl.create 91 in
+    let eq_syms = ref [] in
+    let atom = 
+      rel_atom_of_z3 srk rel_ctx fresh_index_map rsym_to_int names z3pred 
+    in
+    BatHashtbl.iter (fun ind sym ->
+        if BatHashtbl.mem ind_to_sym ind 
+        then eq_syms := ((BatHashtbl.find ind_to_sym ind), sym) :: !eq_syms
+        else BatHashtbl.add ind_to_sym ind sym)
+      fresh_index_map;
+    atom, !eq_syms
+
+
+  let parse_z3fp ?(z3queries=[]) srk rel_ctx z3fp =
+    let rsym_to_int = BatHashtbl.create 91 in
+    let decl_kind e = Z3.FuncDecl.get_decl_kind (Z3.Expr.get_func_decl e) in
+    let parse_rule rule =
+      let names, matrix =
+        if Z3.AST.is_quantifier (Z3.Expr.ast_of_expr rule) then
+          let q = Z3.Quantifier.quantifier_of_expr rule in
+          BatArray.of_list (Z3.Quantifier.get_bound_variable_names q), 
+          Z3.Quantifier.get_body q
+        else BatArray.init 0 (fun _ -> failwith "empty array"), rule
+      in
+      let decl =  decl_kind matrix in
+      let args = Z3.Expr.get_args matrix in
+      begin match decl, args with
+        | (OP_IMPLIES, [hypo;conc]) ->
+          let ind_to_sym = BatHashtbl.create 91 in
+          let hypo_decl = decl_kind hypo in
+          let (atoms, phi) = 
+            begin match hypo_decl with
+              | OP_AND ->
+                let (rels, phis) = 
+                  List.partition 
+                    (fun arg -> decl_kind arg = OP_UNINTERPRETED)
+                    (Z3.Expr.get_args hypo) 
+                in
+                let rel_atoms = 
+                  List.map 
+                    (rel_atom_of_z3 srk rel_ctx ind_to_sym rsym_to_int names)
+                    rels
+                in
+                let phis = List.map (SrkZ3.formula_of_z3 srk) phis in
+                rel_atoms, (mk_and srk phis)
+              | OP_UNINTERPRETED -> 
+                [rel_atom_of_z3 srk rel_ctx ind_to_sym rsym_to_int names hypo],
+                mk_true srk
+              | OP_OR -> failwith "TODO: OR Case"
+              | _ -> [], SrkZ3.formula_of_z3 srk hypo
+            end
+          in
+          let conc, eq_syms = 
+            rel_atom_of_z3_fresh srk rel_ctx ind_to_sym rsym_to_int names conc 
+          in
+          let phi = mk_and 
+              srk 
+              ((substitute 
+                  srk 
+                  (fun ind -> 
+                     if BatHashtbl.mem ind_to_sym ind 
+                     then mk_const srk (BatHashtbl.find ind_to_sym ind)
+                     else failwith "Free var in rule formula not bound in rel")
+                  phi)
+               :: (List.map 
+                     (fun (sym1, sym2) -> mk_eq_syms srk sym1 sym2) 
+                     eq_syms))
+          in
+          (atoms, phi), conc
+        | (OP_UNINTERPRETED, _) ->
+          let hypo = [], mk_true srk in
+          let ind_to_sym = BatHashtbl.create 91 in
+          let conc = 
+            rel_atom_of_z3 srk rel_ctx ind_to_sym rsym_to_int names matrix 
+          in
+          hypo, conc
+        | _ -> failwith "Rule not well formed"
+      end
+    in
+    let parse_query query =
+      Hashtbl.find 
+        rsym_to_int
+        (Z3.Symbol.to_string 
+           (Z3.FuncDecl.get_name (Z3.Expr.get_func_decl query)))
+    in
+    let rules_list = List.map parse_rule (Z3.Fixedpoint.get_rules z3fp) in
+    let queries_list = List.map parse_query z3queries in
+    let rule_counter = ref (List.length rules_list) in
+    let query_counter = ref (List.length queries_list) in
+    let rules = Hashtbl.create !rule_counter in
+    let queries = Hashtbl.create !query_counter in
+    List.iteri (fun ind rule -> Hashtbl.add rules ind rule) rules_list;
+    List.iteri (fun ind query -> Hashtbl.add queries ind query) queries_list;
+    {rules; rule_counter; queries; query_counter}
+
+  let parse_file ?(context=Z3.mk_context []) srk rel_ctx filename =
+    let z3 = context in
+    let fp = Z3.Fixedpoint.mk_fixedpoint z3 in
+    let z3queries = Z3.Fixedpoint.parse_file fp filename in
+    parse_z3fp ~z3queries srk rel_ctx fp
+
+  let parse_string ?(context=Z3.mk_context []) srk rel_ctx filename =
+    let z3 = context in
+    let fp = Z3.Fixedpoint.mk_fixedpoint z3 in
+    let z3queries = Z3.Fixedpoint.parse_string fp filename in
+    parse_z3fp ~z3queries srk rel_ctx fp
+
+
+end
+
+(*
 let show_rel rel = "R_"^(rel)
 let show_rel_atom srk (rel, symbols) =
   let show_symbol srk sym = 
@@ -51,126 +299,9 @@ let show_rule srk (hypo,conc) =
 
 let show_chc srk chc =
   List.fold_left (fun str rule -> str^"\n"^(show_rule srk rule)) "" chc.rules
+*)
 
-let typ_of_sort sort =
-  let open Z3enums in
-  match Z3.Sort.get_sort_kind sort with
-  | REAL_SORT -> `TyReal
-  | INT_SORT -> `TyInt
-  | BOOL_SORT -> `TyBool
-  | _ -> failwith "TODO: allow function types"
-
-let rl_atom_of_z3prd srk ?(ind_to_sym=BatHashtbl.create 100) names z3prd =
-  let decl = Z3.Expr.get_func_decl z3prd in
-  let rel = Z3.Symbol.to_string (Z3.FuncDecl.get_name decl) in
-  let args = List.map 
-      (fun arg -> 
-         let index = Z3.Quantifier.get_index arg in
-         if BatHashtbl.mem ind_to_sym index then
-           BatHashtbl.find ind_to_sym index
-         else (
-           let sort = typ_of_sort (Z3.Expr.get_sort arg) in
-           let sym = 
-             mk_symbol srk ~name:(Z3.Symbol.to_string names.(index)) sort 
-           in
-           BatHashtbl.add ind_to_sym index sym;
-           sym)) 
-      (Z3.Expr.get_args z3prd) in
-  rel, args
-
-let rl_atom_of_z3prd_fresh srk ?(ind_to_sym=BatHashtbl.create 100) names z3prd =
-  let fresh_index_map = BatHashtbl.create 100 in
-  let eq_syms = ref [] in
-  let atom = rl_atom_of_z3prd srk ~ind_to_sym:fresh_index_map names z3prd in
-  BatHashtbl.iter (fun ind sym ->
-      if BatHashtbl.mem ind_to_sym ind 
-      then eq_syms := ((BatHashtbl.find ind_to_sym ind), sym) :: !eq_syms
-      else BatHashtbl.add ind_to_sym ind sym)
-    fresh_index_map;
-  atom, !eq_syms
-
-let parse_file ?(context=Z3.mk_context []) srk filename =
-  let z3 = context in
-  let fp = Z3.Fixedpoint.mk_fixedpoint z3 in
-  let z3queries = Z3.Fixedpoint.parse_file fp filename in
-  let is_rel_atom z3expr = 
-    Z3.FuncDecl.get_decl_kind (Z3.Expr.get_func_decl z3expr) = OP_UNINTERPRETED
-  in
-  let parse_rule rule =
-    let names, matrix =
-      if Z3.AST.is_quantifier (Z3.Expr.ast_of_expr rule) then
-        let q = Z3.Quantifier.quantifier_of_expr rule in
-        BatArray.of_list (Z3.Quantifier.get_bound_variable_names q), 
-        Z3.Quantifier.get_body q
-      else BatArray.init 0 (fun _ -> failwith "empty array"), rule
-    in
-    (* Note that we don't substitute phi with ind_to_sym in this function *)
-    let parse_hypothesis ?(ind_to_sym=BatHashtbl.create 100) expr = 
-      let decl =  Z3.FuncDecl.get_decl_kind (Z3.Expr.get_func_decl expr) in
-      begin match decl with
-        | OP_AND ->
-          let (rels, phis) = 
-            List.partition (is_rel_atom) (Z3.Expr.get_args expr) 
-          in
-          let rel_atoms = 
-            List.map (rl_atom_of_z3prd ~ind_to_sym srk names) rels
-          in
-          let phis = List.map (SrkZ3.formula_of_z3 srk) phis in
-          rel_atoms, (mk_and srk phis)
-        | OP_UNINTERPRETED -> [rl_atom_of_z3prd srk names expr], mk_true srk
-        | OP_OR -> failwith "TODO: OR Case"
-        | _ -> [], SrkZ3.formula_of_z3 srk expr
-      end
-    in
-    let decl =  Z3.FuncDecl.get_decl_kind (Z3.Expr.get_func_decl matrix) in
-    let args = Z3.Expr.get_args matrix in
-    begin match decl, args with
-      | (OP_IMPLIES, [hypo;conc]) ->
-        let ind_to_sym = BatHashtbl.create 100 in
-        let (atoms, phi) = parse_hypothesis ~ind_to_sym hypo in
-        let conc, eq_syms = rl_atom_of_z3prd_fresh srk ~ind_to_sym names conc in
-        let phi = mk_and 
-            srk 
-            ((substitute 
-               srk 
-               (fun ind -> 
-                  if BatHashtbl.mem ind_to_sym ind 
-                  then mk_const srk (BatHashtbl.find ind_to_sym ind)
-                  else failwith "Free var in rule formula not bound in rel")
-               phi)
-             :: (List.map 
-                   (fun (sym1, sym2) -> mk_eq_syms srk sym1 sym2) 
-                   eq_syms))
-        in
-        (atoms, phi), conc
-      | (OP_UNINTERPRETED, _) ->
-        let hypo = [], mk_true srk in
-        let conc = rl_atom_of_z3prd srk names matrix in
-        hypo, conc
-      | _ -> failwith "Rule not well formed"
-    end
-  in
-  let parse_query query =
-    Z3.Symbol.to_string (Z3.FuncDecl.get_name (Z3.Expr.get_func_decl query))
-  in
-  let rules = List.map parse_rule (Z3.Fixedpoint.get_rules fp) in
-  let queries = List.map parse_query z3queries in
-  {rules; queries}
-
-
-let get_rel_syms chc =
-  let rset = 
-    List.fold_left 
-      (fun rset ((rel_atom_lst, _), conc) ->
-         List.fold_left 
-           (fun rset rel_atom -> RSet.add (rel_sym_of rel_atom) rset)
-           (RSet.add (rel_sym_of conc) rset)
-           rel_atom_lst)
-      (RSet.of_list chc.queries)
-      chc.rules
-  in
-  rset
-
+(*
 let to_weighted_graph srk chc pd =
   let open WeightedGraph in
   let emptyarr = BatArray.init 0 (fun _ -> failwith "empty") in
@@ -285,3 +416,4 @@ let to_weighted_graph srk chc pd =
       chc.queries
   in
   wg, true_vert, goal_vert
+   *)
--- srk/src/chc.mli
+++ srk/src/chc.mli
@@ -1,27 +1,90 @@
 (* Constrainted horn clauses *)
 open Syntax
 
-type 'a chccontext 
+type 'b relcontext
 type relation
-type relation_atom = relation * symbol list
-type 'a hypothesis = relation_atom list * 'a formula
-type conclusion = relation_atom
-type 'a rule = 'a hypothesis * conclusion
-type 'a query = relation
-type 'a chc = {rules : 'a rule list; queries : 'a query list}
+type 'b relation_atom = relation * symbol list
+type ('a, 'b) hypothesis = 'b relation_atom list * 'a formula
+type ('a, 'b) rule = ('a, 'b) hypothesis * 'b relation_atom
+type ('a, 'b) query = relation
+type 'c rule_name
+type 'c query_name
+type ('a, 'b, 'c) chc
 
-val parse_file : ?context:Z3.context -> 'a Syntax.context -> string -> 'a chc
-val show_rel : relation -> string
+(*val parse_file : ?context:Z3.context -> 'a Syntax.context -> string -> 'a chc*)
+(*val show_rel : relation -> string
 val show_rel_atom : 'a context -> relation_atom -> string
 val show_hypothesis : 'a context -> 'a hypothesis -> string
 val show_conclusion : 'a context -> conclusion -> string
 val show_rule : 'a context -> 'a rule -> string
 val show_chc : 'a context -> 'a chc -> string
-val rel_sym_of : relation_atom -> relation
-val params_of : relation_atom -> symbol list
-val to_weighted_graph : 'a context -> 
+*)
+val mk_relcontext : 'b relcontext
+val pp_relation_atom : 
+  'a context -> 'b relcontext -> Format.formatter -> 'b relation_atom -> unit
+val pp_hypothesis : 
+  'a context -> 'b relcontext -> Format.formatter -> ('a, 'b) hypothesis -> unit
+val pp_rule : 
+  'a context -> 'b relcontext -> Format.formatter -> ('a, 'b) rule -> unit
+val pp_query : 
+  'a context -> 'b relcontext -> Format.formatter -> ('a, 'b) query -> unit
+
+val rel_sym_of : 'b relation_atom -> relation
+val params_of : 'b relation_atom -> symbol list
+
+module Relation : sig
+  type t = relation
+  module Set : BatSet.S with type elt = relation
+  val compare : t -> t -> int
+  val mk_relation : 'b relcontext -> ?name:string -> typ list -> relation
+  val type_of : 'b relcontext -> relation -> typ list
+  val name_of : 'b relcontext -> relation -> string
+  val pp : 'b relcontext -> Format.formatter -> relation -> unit
+end
+module Chc : sig
+  type ('a, 'b, 'c) t = ('a, 'b, 'c) chc
+  val create : ?rlength:int -> ?qlength:int -> unit -> ('a, 'b, 'c) chc
+  val copy : ('a, 'b, 'c) chc -> ('a, 'b, 'c) chc
+  val add_rule : ('a, 'b, 'c) chc -> ('a, 'b) rule -> 'c rule_name
+  val add_query : ('a, 'b, 'c) chc -> ('a, 'b) query -> 'c query_name
+  val del_rule : ('a, 'b, 'c) chc -> 'c rule_name -> bool
+  val del_query : ('a, 'b, 'c) chc -> 'c query_name -> bool
+  val get_rules : ('a, 'b, 'c) chc -> ('c rule_name, ('a, 'b) rule) Hashtbl.t
+  val get_queries :
+    ('a, 'b, 'c) chc -> ('c query_name, ('a, 'b) query) Hashtbl.t
+  val get_rule : ('a, 'b, 'c) chc -> 'c rule_name -> ('a, 'b) rule
+  val get_query : ('a, 'b, 'c) chc -> 'c query_name -> ('a, 'b) query
+  val upd_rule : ('a, 'b, 'c) chc -> 'c rule_name -> ('a, 'b) rule -> unit
+  val upd_query : ('a, 'b, 'c) chc -> 'c query_name -> ('a, 'b) query -> unit
+  val get_relations_used : ('a, 'b, 'c) chc -> Relation.Set.t
+  val pp : 
+    'a context -> 'b relcontext -> Format.formatter -> ('a, 'b, 'c) chc -> unit
+  val is_linear : ('a, 'b, 'c) chc -> bool
+end
+
+module ChcSrkZ3 : sig
+  val parse_z3fp : 
+    ?z3queries:Z3.Expr.expr list -> 
+    'a context -> 
+    'b relcontext ->
+    Z3.Fixedpoint.fixedpoint ->
+    ('a, 'b, 'c) chc
+  val parse_file :
+    ?context:Z3.context ->
+    'a context ->
+    'b relcontext ->
+    string -> 
+    ('a, 'b, 'c) chc
+  val parse_string :
+    ?context:Z3.context ->
+    'a context ->
+    'b relcontext ->
+    string -> 
+    ('a, 'b, 'c) chc
+end
+(*val to_weighted_graph : 'a context -> 
   'a chc -> 
   (module Iteration.PreDomain) -> 
   (Syntax.symbol array * Syntax.symbol array * 'a Syntax.Formula.t) 
     WeightedGraph.t * int * int
-
+*)
