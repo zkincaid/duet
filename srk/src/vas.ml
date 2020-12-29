f97cefd45775faff7e5108e5db3004a62fab6d86
JakeSilverman <JakeRossSilverman@gmail.com>
2020-12-29 14:09:21 -0500

    undo changes to make vas int

--- srk/src/vas.ml
+++ srk/src/vas.ml
@@ -131,7 +131,7 @@ let create_exp_vars srk s_lst transformers =
 
       (*Create S vars*)
       let svar =
-        mk_constants (M.nb_rows hd) ("S" ^ (string_of_int (List.length s_lst))) `TyInt
+        mk_constants (M.nb_rows hd) ("S" ^ (string_of_int (List.length s_lst))) `TyReal
       in
 
       (*Group vars together*)
@@ -346,9 +346,7 @@ let exp srk tr_symbols loop_counter vabs =
         loop_counter s_lst (TSet.to_list v) in
     let constr1 = exp_sx_constraints srk coh_class_pairs
         (TSet.to_list v) kvarst ksumst (unify s_lst) tr_symbols in
-    let r = mk_and srk [formula; constr1] in
-    Log.errorf "result is %a" (Formula.pp srk) r;
-    r
+    mk_and srk [formula; constr1]
   )
 
 
@@ -517,7 +515,6 @@ let abstract srk tf =
   Smt.Solver.add solver [phi];
   let {v;s_lst} = go (mk_bottom tr_symbols) in
   let result = {v;s_lst} in
-  Log.errorf "\n\nvas is %a\n\n" (Formula.pp srk) (gamma srk result tr_symbols);
   result
 
 module Monotone = struct
