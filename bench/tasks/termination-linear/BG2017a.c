f0968100d5d0e0d48ce7245b6fa4d7da62fcab48
Zachary Kincaid <zkincaid@gmail.com>
2021-01-29 16:51:14 -0500

    Benchmarks from Ben-Amram & Genaim, CAV 2017

---
+++ bench/tasks/termination-linear/BG2017a.c
@@ -0,0 +1,16 @@
+// Source: Amir M. Ben-Amram, Samir Genaim, "On Multiphase-Linear
+// Ranking Functions", CAV 2017.
+
+
+extern int __VERIFIER_nondet_int(void);
+
+int main(int argc, char* argv[]) {
+  int x = __VERIFIER_nondet_int();
+  int y = __VERIFIER_nondet_int();
+  int z = __VERIFIER_nondet_int();
+  while (x >= -z) {
+    x = x + y;
+    y = y + z;
+    z = z - 1;
+  }
+}
---
+++ bench/tasks/termination-linear/BG2017b.c
@@ -0,0 +1,21 @@
+// Source: Amir M. Ben-Amram, Samir Genaim, "On Multiphase-Linear
+// Ranking Functions", CAV 2017.
+
+// Comments: does not terminate over the rationals
+
+extern int __VERIFIER_nondet_int(void);
+
+int main(int argc, char* argv[]) {
+  int x1 = __VERIFIER_nondet_int();
+  int x2 = __VERIFIER_nondet_int();
+
+  // avoid overflow
+  if (x1 > 1000 || x2 > 1000) return 0;
+  if (x1 < -1000 || x2 < -1000) return 0;
+
+
+  while (x2 - x1 <= 0 && x1 + x2 >= 1) {
+    x2 = x2 - 2 * x1 + 1;
+  }
+  return 0;
+}
