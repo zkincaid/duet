ec7dc134b65e49f41f1bb8cc9caeba72b82e27b9
Shaowei Zhu <shaoweiz@cs.princeton.edu>
2022-01-14 03:08:20 -0500

    benchmark defs for weak theory + cra

---
+++ bench/tools/crawt.py
@@ -0,0 +1,28 @@
+import benchexec.util as util
+import benchexec.tools.template
+import benchexec.result as result
+
+class Tool(benchexec.tools.template.BaseTool):
+    """
+    Wrapper for CRA
+    """
+
+    def executable(self):
+        return util.find_executable("duet.exe")
+
+    def name(self):
+        return "WeakTheory"
+
+    def determine_result(self, returncode, returnsignal, output, isTimeout):
+        output = "\n".join(output)
+        if ((returnsignal == 9) or (returnsignal == 15)) and isTimeout:
+            status = "TIMEOUT"
+        elif returnsignal == 9:
+            status = "KILLED BY SIGNAL 9"
+        elif "0 errors total" in output:
+            status = result.RESULT_TRUE_PROP
+        elif returncode != 0:
+            status = "ERROR ({0})".format(returncode)
+        else:
+            status = result.RESULT_UNKNOWN
+        return status
