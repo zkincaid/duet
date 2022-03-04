14446bef4a9e7d7a6c41cbde22da886e2c84aa15
Nicolas Koh <ckoh@princeton.edu>
2022-03-03 23:07:28 -0500

    Nit

--- srk/src/srkZ3.ml
+++ srk/src/srkZ3.ml
@@ -68,6 +68,10 @@ let rec eval alg ast =
       let args = List.map (eval alg) (Expr.get_args ast) in
       match FuncDecl.get_decl_kind decl, args with
       | (OP_UNINTERPRETED, args) ->
+         (* TODO: Whether it is interpreted or not depends on the theory chosen
+            for the SMT solver. For now, whether it is interpreted or not,
+            "is_int" is translated into `IsInt.
+          *)
          let name = decl |> FuncDecl.get_name |> Symbol.get_string in
          if String.equal name "is_int" then
            begin
@@ -103,6 +107,7 @@ let rec eval alg ast =
       | (OP_LT, [s; t]) -> alg (`Atom (`Lt, s, t))
       | (OP_GT, [s; t]) -> alg (`Atom (`Lt, t, s))
       | (OP_ITE, [cond; s; t]) -> alg (`Ite (cond, s, t))
+      | (OP_IS_INT, [s]) -> alg (`IsInt [s])
       | (_, _) -> invalid_arg ("eval: unknown application: "
                                ^ (Expr.to_string ast))
     end
@@ -290,8 +295,11 @@ and z3_of_formula srk z3 =
     | `Proposition (`Var i) ->
        Z3.Quantifier.mk_bound z3 i (sort_of_typ z3 `TyBool)
     | `Atom (`IsInt s) ->
-       let latgen_sym = get_named_symbol srk "is_int" in
-       let decl = decl_of_symbol z3 srk latgen_sym in
+       (* TODO: Check that this is okay whether is_int is interpreted in the solver's
+          logic or not.
+        *)
+       let is_int_sym = get_named_symbol srk "is_int" in
+       let decl = decl_of_symbol z3 srk is_int_sym in
        Z3.Expr.mk_app z3 decl [z3_of_expr srk z3 (s :> ('a, typ_fo) expr)]
     | `Proposition (`App (p, [])) ->
       let decl =
