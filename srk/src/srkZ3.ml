0198f9834a85ee03554547a542ad02b7487829e3
Zachary Kincaid <zkincaid@gmail.com>
2022-06-20 16:24:46 -0400

    Translation for distinct and xor

--- srk/src/srkZ3.ml
+++ srk/src/srkZ3.ml
@@ -95,6 +95,25 @@ let rec eval alg ast =
       | (OP_LT, [s; t]) -> alg (`Atom (`Lt, s, t))
       | (OP_GT, [s; t]) -> alg (`Atom (`Lt, t, s))
       | (OP_ITE, [cond; s; t]) -> alg (`Ite (cond, s, t))
+      | (OP_DISTINCT, xs) ->
+        let rec exclusive acc = function
+          | y::ys ->
+            exclusive
+              (List.fold_left
+                 (fun acc z -> alg (`Not (alg (`And [y; z])))::acc)
+                 acc
+                 ys)
+              ys
+          | [] -> acc
+        in
+        alg (`And (exclusive [alg (`Or xs)] xs))
+      | (OP_XOR, xs) ->
+        let xor x y =
+          alg (`Or [alg (`And [alg (`Not x); y]);
+                    alg (`And [x; alg (`Not y)])])
+        in
+        BatList.reduce xor xs
+
       | (_, _) -> invalid_arg ("eval: unknown application: "
                                ^ (Expr.to_string ast))
     end
