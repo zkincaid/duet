4ef35f03d0d1f76065cc39ec6ec209a51e7bbc52
Nicolas Koh <ckoh@princeton.edu>
2022-07-28 22:18:56 -0400

    Test polyhedron: integer hull

--- srk/test/test_polyhedron.ml
+++ srk/test/test_polyhedron.ml
@@ -11,16 +11,57 @@ let mk_polyhedron halfspaces =
                     (V.add_term (QQ.of_int (-a)) Linear.const_dim (mk_vector v))))
   |> Polyhedron.of_constraints
 
-
-let mk_polyhedron_generators dim vertices rays =
+let mk_polyhedron_from_generators mk_vector dim vertices rays =
   (List.map (fun v -> (`Vertex, mk_vector v)) vertices)
   @(List.map (fun v -> (`Ray, mk_vector v)) rays)
   |> BatList.enum
   |> Polyhedron.of_generators dim
-  
+
+let mk_polyhedron_generators =
+  mk_polyhedron_from_generators mk_vector
+
+let mk_polyhedron_rational_generators dim vertices rays =
+  let qqify v = List.map (fun (a, b) -> QQ.of_frac a b) v in
+  mk_polyhedron_from_generators mk_qqvector dim
+    (List.map qqify vertices)
+    (List.map qqify rays)
 
 let assert_equal_polyhedron p q =
   assert_equal ~cmp:Polyhedron.equal p q
+
+let test_vertical_integer_hull k () =
+  (*
+       x >= 0
+       x + ky <= k
+       x - ky <= 0
+   *)
+  let p = mk_polyhedron [ ([1; 0], 0)
+                        ; ([-1; -k], -k)
+                        ; ([-1; k], 0) ] in
+  let hull_gc = Polyhedron.integer_hull `GomoryChvatal p in
+  let hull_normaliz = Polyhedron.integer_hull `Normaliz p in
+  let expected_hull = mk_polyhedron [ ([1; 0], 0)
+                                    ; ([-1; 0], 0)
+                                    ; ([0; 1], 0)
+                                    ; ([0; -1], -1) ] in
+  assert_equal ~cmp:Polyhedron.equal hull_gc expected_hull;
+  assert_equal ~cmp:Polyhedron.equal hull_normaliz expected_hull
+
+let test_translated_parallelogram height () =
+  let p = mk_polyhedron_rational_generators 2
+            [ [(1, 2); (0, 1)]
+            ; [(3, 2); (0, 1)]
+            ; [(3, 2); (height, 1)]
+            ; [(5, 2); (height, 1)] ] []
+  in
+  let hull_gc = Polyhedron.integer_hull `GomoryChvatal p in
+  let hull_normaliz = Polyhedron.integer_hull `Normaliz p in
+  let expected_hull =
+    mk_polyhedron_generators 2
+      [ [1; 0]; [1; height/2]; [2; height - (height/2)]; [2; height] ] []
+  in
+  assert_equal ~cmp:Polyhedron.equal hull_gc expected_hull;
+  assert_equal ~cmp:Polyhedron.equal hull_normaliz expected_hull
 
 let suite = "Polyhedron" >::: [
       "equal1" >:: (fun () ->
@@ -47,7 +88,7 @@ let suite = "Polyhedron" >::: [
                          ([1; 1; 0], 2)]
         in
         assert_bool "Disequal constraint space" (not (Polyhedron.equal p q)));
-            
+
       "disequal2" >:: (fun () ->
         let p =
           mk_polyhedron [([1; 0; -1], 0);
@@ -176,5 +217,8 @@ let suite = "Polyhedron" >::: [
              [2; 4]]
             []
         in
-        assert_equal_polyhedron p q)
+        assert_equal_polyhedron p q);
+
+      "integer_hull_1" >:: test_vertical_integer_hull 3;
+      "integer_hull_2" >:: test_translated_parallelogram 3;
   ]
