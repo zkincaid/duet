b54918578b261fc55396a1f8584f99ac085294ec
Shaowei Zhu <shaoweiz@cs.princeton.edu>
2022-09-29 21:27:45 -0400

    bench script that calculates mean and max variance over multiple runs

--- bench/bench.py
+++ bench/bench.py
@@ -8,6 +8,8 @@ import subprocess
 import tempfile
 import types
 import statistics
+from collections import defaultdict
+from tabulate import tabulate
 
 # Configuration -- can be reconfigured via the command line
 tools = ['ComPACT','CPAchecker','UAutomizer','2ls','Termite']
@@ -15,6 +17,7 @@ suites = ['Termination','bitprecise','recursive','polybench']
 timeout = 600
 cache = True
 replace_cached = False
+num_runs = 1
 
 table_begin = """<?xml version="1.0" ?>
 <!DOCTYPE table PUBLIC "+//IDN sosy-lab.org//DTD BenchExec table 1.10//EN" "https://www.sosy-lab.org/benchexec/table-1.10.dtd">
@@ -68,56 +71,211 @@ def run():
                                 env=my_env)
 
 
-def recent_result(tool, suite):
+def recent_result(tool, suite, which_run=1):
     results = glob.glob("results/%s.*.%s.xml.bz2" % (tool, suite))
     results.sort()
     if len(results) == 0:
         print("No results for %s on suite %s" % (tool, suite))
         exit(-1)
     else:
-        return results[-1]
+        return results[-which_run]
 
-def recent_result_data(tools, suites):
-    data = []
-    for suite in suites:
-        with tempfile.TemporaryDirectory() as tmp_dir:
-            p = subprocess.run(['table-generator',
+def recent_result_data(tools, suites, num_runs_to_fetch=1):
+    multirun_data = []
+    for run in range(num_runs_to_fetch):
+        data = []
+        for suite in suites:
+            with tempfile.TemporaryDirectory() as tmp_dir:
+                p = subprocess.run(['table-generator',
                                 '-f', 'csv',
                                 '-o', tmp_dir,
                                 '-x', 'simplecsv.xml',
                                 '-q']
-                               + list(map(lambda x: recent_result(x, suite), tools)))
-            table = os.path.join(tmp_dir, "simplecsv.table.csv")
-            # strip 3 rows of header info
-            data += list(map(lambda row: row.rstrip().split('\t'),
-                             open(table).readlines()))[3:]
-    return data
+                            + list(map(lambda x: recent_result(x, suite, which_run=run), tools)))
+                table = os.path.join(tmp_dir, "simplecsv.table.csv")
+                # strip 3 rows of header info
+                data += list(map(lambda row: row.rstrip().split('\t'),
+                                open(table).readlines()))[3:]
+        multirun_data.append(data)
+    return multirun_data
     
-def summarize_result(tool, suite):
-    data = recent_result_data([tool],[suite])
-    result = types.SimpleNamespace()
-    result.total = 0
-    result.time = 0
-    result.correct = 0
-    result.timeout = 0
-    result.unknown = 0
-    result.times_excluding_timeouts = []
-    for entry in data:
-        result.total += 1
-        result.time += get_time(entry, 0)
-        if (get_result(entry, 0) == "TIMEOUT"):
-            result.timeout += 1
-        elif (get_category(entry, 0) == "correct"):
-            result.correct += 1
-            result.times_excluding_timeouts.append(get_time(entry, 0))
-        elif (get_category(entry, 0) == "unknown"):
-            result.unknown += 1
-            result.times_excluding_timeouts.append(get_time(entry, 0))
-        else:
-            result.times_excluding_timeouts.append(get_time(entry, 0))
-    return result
+def summarize_result(tool, suite, average_over_runs=1):
+    multirun_data = recent_result_data([tool],[suite], num_runs_to_fetch=average_over_runs)
+    multirun_results = []
+    for run in range(average_over_runs):
+    # data = recent_result_data([tool],[suite])
+        data = multirun_data[run]
+        result = types.SimpleNamespace()
+        result.total = 0
+        result.time = 0
+        result.maxvar = 0
+        result.correct = 0
+        result.timeout = 0
+        result.memout = 0
+        result.unknown = 0
+        # result.times_excluding_timeouts = []
+        result.time_by_verdict = defaultdict(float) 
+        result.task_times_by_verdict = defaultdict(list) 
+        result.maxvar_by_verdict = defaultdict(float)
+        result.num_by_verdict = defaultdict(int)
+        result.correct_by_verdict = defaultdict(int) 
+        result.timeout_by_verdict = defaultdict(int)
+        result.memout_by_verdict = defaultdict(int)
+        for entry in data:
+            result.total += 1
+            if len(entry) > 4:
+                result.time += float(entry[4])
+                v = entry[1]
+                if v == '':
+                    v = 'unknown'
+                result.num_by_verdict[v] += 1
+                t = float(entry[4])
+                result.time_by_verdict[v] = result.time_by_verdict[v] + t 
+                result.task_times_by_verdict[v].append(t)
+                if entry[2] == 'TIMEOUT':
+                    result.timeout += 1
+                    result.timeout_by_verdict[v] = result.timeout_by_verdict[v] + 1 
+                else:
+                    # result.time_by_verdict[v] = result.time_by_verdict[v] + t 
+                    if entry[3] == 'correct':
+                        result.correct += 1
+                        result.correct_by_verdict[v] = result.correct_by_verdict[v] + 1
+                    elif entry[2] == 'KILLED BY SIGNAL 9':
+                        result.memout += 1
+                        result.memout_by_verdict[v] = result.memout_by_verdict[v] + 1
+                    elif entry[3] == "unknown":
+                        result.unknown += 1
+                continue
+            result.time += get_time(entry, 0)
+            if (get_result(entry, 0) == "TIMEOUT"):
+                result.timeout += 1
+            elif (get_category(entry, 0) == "correct"):
+                result.correct += 1
+            elif (get_category(entry, 0) == "unknown"):
+                result.unknown += 1
+        multirun_results.append(result)
+    ar = types.SimpleNamespace()
+    r = multirun_results[0]
+    ar.total = r.total
 
-def summary():
+
+    multi_run_times = []
+    for run in range(average_over_runs):
+        multi_run_times.append(multirun_results[run].time)
+    mean_run_time = statistics.mean(multi_run_times)
+    ar.time = mean_run_time
+
+    max_time_var = 0.0
+    for task_id in range(len(r)):
+        times_per_task = []
+        for run in range(average_over_runs):
+            times_per_task.append(float(multirun_results[run][task_id][4]))
+        var_this_task = statistics.variance(times_per_task)
+        max_time_var = max(max_time_var, var_this_task)
+    ar.maxvar = max_time_var
+
+    ar.correct = r.correct
+    ar.timeout = r.timeout
+    ar.memout = r.memout
+    ar.unknown = r.unknown
+
+    ar.time_by_verdict = defaultdict(float) 
+    ar.maxvar_by_verdict = defaultdict(float)
+    for v in ['true', 'false', 'unknown']:
+        multi_run_times = []
+        for run in range(average_over_runs):
+            multi_run_times.append(multirun_results[run].time_by_verdict[v])
+        mean_run_time = statistics.mean(multi_run_times)
+        ar.time_by_verdict[v] = mean_run_time
+
+        max_time_var = 0.0
+        for task_id in range(len(r.task_times_by_verdict[v])):
+            times_per_task = []
+            for run in range(average_over_runs):
+                times_per_task.append(multirun_results[run][task_id])
+            var_this_task = statistics.variance(times_per_task)
+            max_time_var = max(max_time_var, var_this_task)
+        ar.maxvar_by_verdict[v] = max_time_var
+
+    ar.num_by_verdict = r.num_by_verdict
+    ar.correct_by_verdict = r.correct_by_verdict
+    ar.timeout_by_verdict = r.timeout_by_verdict
+    ar.memout_by_verdict = r.memout_by_verdict
+    return ar
+
+def summary_by_verdict(average_over_runs=1):
+    res = {}
+
+    
+    num = {}
+    total_correct = {}
+    total_time = {}
+    num_timeout = {}
+    num_memout = {}
+    max_variance = {}
+
+    for tool in tools:
+        total_correct[tool] = 0
+        num_timeout[tool] = 0
+        num_memout[tool] = 0
+        total_time[tool] = 0
+        max_variance[tool] = 0
+
+    for s in suites:
+        r = {}
+        for t in tools:
+            r[t] = summarize_result(t, s)
+        res[s] = r 
+
+    for suite in suites:
+        for verd in ['true', 'false', 'unknown']:
+            row_name = suite + ' - ' + verd
+            for tool in tools:
+                r = res[suite][tool] 
+                num_suite = r.num_by_verdict[verd]
+                total_correct[tool] += r.correct_by_verdict[verd]
+                total_time[tool] += r.time_by_verdict[verd]
+                max_variance[tool] = max(max_variance[tool]. r.maxvar)
+                num_timeout[tool] += r.timeout_by_verdict[verd]
+                num_memout[tool] += r.memout_by_verdict[verd]
+            num[row_name] = num_suite
+
+    print("\\begin{tabular}{@{}lc|%s@{}}" % ("|".join(["c@{}c@{}r@{}r"] * (len(tools)))))
+    print("\\toprule")
+    print(" &",end='')
+    for tool in tools[:-1]:
+        print(" & \\multicolumn{4}{c|}{%s}" % tool, end='')
+    print(" & \\multicolumn{4}{c}{%s}\\\\" % tools[-1])
+
+    print(" & \#tasks & %s\\\\\\midrule" % " & ".join(["\#P & \#E & t & $\sigma$ "] * len(tools)))
+
+    for suite in suites:
+        for verd in ['true', 'false', 'unknown']:
+            suite_name = suite + ' - ' + verd
+            print("%s & %d" % (suite_name, num[suite_name]),end='')
+            for tool in tools:
+                c = res[suite][tool]
+                print(" & %d" % c.correct_by_verdict[verd],end='')
+                print(" & %d/%d" % (c.timeout_by_verdict[verd], c.memout_by_verdict[verd]),end='')
+                print(" & %.1f" % c.time_by_verdict[verd],end='')
+                print(" & %.2f" % c.maxvar_by_verdict[verd],end='')
+
+            print("\\\\")
+    print("\\midrule")
+
+
+    print("Total & %d " % sum(num.values()), end='')
+    for tool in tools:
+        print(" & %d" % total_correct[tool],end='')
+        print(" & %d/%d" % (num_timeout[tool], num_memout[tool]),end='')
+        print(" & %.1f" % total_time[tool],end='')
+        print(" & %.2f" % max_variance[tool],end='')
+    print("\\\\")
+
+    print("\\bottomrule")
+    print("\\end{tabular}")
+
+def summary(average_over_runs=1):
     matrix = {}
 
     best_time = {}
@@ -125,15 +283,13 @@ def summary():
     num = {}
     total_correct = {}
     total_time = {}
+    max_variance = {}
     num_timeout = {}
-    times_excluding_timeout = {}
-    mean_time_excluding_timeout = {}
-    median_time_excluding_timeout = {}
-
+    
     for tool in tools:
         total_correct[tool] = 0
         total_time[tool] = 0
-        times_excluding_timeout[tool] = []
+        max_variance[tool] = 0
         num_timeout[tool] = 0
 
     for suite in suites:
@@ -142,32 +298,31 @@ def summary():
         best_correct_suite = 0
         num_suite = 0
         for tool in tools:
-            r = summarize_result(tool, suite)
-            best_time_suite = min(best_time_suite, r.time)
+            r = summarize_result(tool, suite, average_over_runs=average_over_runs)
+
             best_correct_suite = max(best_correct_suite, r.correct)
             num_suite = r.total
             row[tool] = r
             total_correct[tool] += r.correct
+            
             total_time[tool] += r.time
-            times_excluding_timeout[tool].extend(r.times_excluding_timeouts)
+            best_time_suite = min(best_time_suite, r.time)
+            max_variance[tool] = max(max_variance[tool]. r.maxvar)
             num_timeout[tool] += r.timeout
+
         best_time[suite] = best_time_suite
         best_correct[suite] = best_correct_suite
         num[suite] = num_suite
         matrix[suite] = row
 
-    for tool in tools:
-        mean_time_excluding_timeout[tool] = statistics.mean(times_excluding_timeout[tool])
-        median_time_excluding_timeout[tool] = statistics.median(times_excluding_timeout[tool])
-
-    print("\\begin{tabular}{@{}lc|%s@{}}" % ("|".join(["c@{}r"] * (len(tools)))))
+    print("\\begin{tabular}{@{}lc|%s@{}}" % ("|".join(["c@{}r@{}r"] * (len(tools)))))
     print("\\toprule")
     print(" &",end='')
     for tool in tools[:-1]:
-        print(" & \\multicolumn{2}{c|}{%s}" % tool, end='')
-    print(" & \\multicolumn{2}{c}{%s}\\\\" % tools[-1])
+        print(" & \\multicolumn{3}{c|}{%s}" % tool, end='')
+    print(" & \\multicolumn{3}{c}{%s}\\\\" % tools[-1])
 
-    print(" & \#tasks & %s\\\\\\midrule" % " & ".join(["\#correct & time"] * len(tools)))
+    print(" & \#tasks & %s\\\\\\midrule" % " & ".join(["\#correct & time & var"] * len(tools)))
 
     for suite in suites:
         print("%s & %d" % (suite, num[suite]),end='')
@@ -195,26 +350,14 @@ def summary():
             print(" & %d" % total_correct[tool],end='')
 
         if (total_time[tool] == best_total_time):
-            print(" & \\textbf{%.1f}" % best_total_time,end='')
+            print(" & \\textbf{%.1f},max $\\sigma$=(%.2f)" % (best_total_time, max_variance[tool]),end='')
         else:
-            print(" & %.1f" % total_time[tool],end='')
+            print(" & %.1f, max $\\sigma$=(%.2f)" % (total_time[tool], max_variance[tool]),end='')
     print("\\\\")
 
     print("Timeouts & ", end='');
     for tool in tools:
         print(" & \\multicolumn{2}{c}{%d}" % num_timeout[tool], end='')
-
-    print("\\\\")
-
-    print("Mean time & ", end='');
-    for tool in tools:
-        print(" & \\multicolumn{2}{c}{%.1f}" % mean_time_excluding_timeout[tool], end='')
-
-    print("\\\\")
-
-    print("Median time & ", end='');
-    for tool in tools:
-        print(" & \\multicolumn{2}{c}{%.1f}" % median_time_excluding_timeout[tool], end='')
 
     print("\\\\")
 
@@ -259,7 +402,7 @@ def cactus_plot():
     data = ""
 
     for tool_name in tools:
-        matrix = recent_result_data([tool_name], suites)
+        matrix = recent_result_data([tool_name], suites)[0]
 
         print ("Writing data to %s.dat" % tool_name)
         f = open("%s.dat" % tool_name, "w")
@@ -281,7 +424,7 @@ def scatter_plot():
         print("For scatter plot, must supply exactly two tools to compare")
         exit(-1)
 
-    matrix = recent_result_data(tools, suites)
+    matrix = recent_result_data(tools, suites)[0]
 
     # Don't include points where one tool gave a wrong answer
     ok_results = ["correct", "TIMEOUT", "unknown"]
@@ -399,6 +542,9 @@ if __name__ == "__main__":
             cache = False
             replace_cached = True
             opts = opts[1:]
+        elif (opts[0] == "--collect-multiple-runs"):
+            num_runs = int(opts[1])
+            opts = opts[2:]
         else:
             print("Unrecognized option: %s" % opts[0])
             exit (-1)
@@ -413,5 +559,7 @@ if __name__ == "__main__":
         scatter_plot()
     elif (command == "summary"):
         summary()
+    elif (command == "summarybyverdict"):
+        summary_by_verdict()
     else:
         print("Unknown command")
