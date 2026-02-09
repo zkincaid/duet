0a37ecf3b3054c25f45cf45d128cbe492b18091a
Nikhil Pimpalkhare <nikhil.pimpalkhare@princeton.edu>
2026-02-09 16:54:56 -0500

    small cleanup

--- srk/src/arraylift.ml
+++ srk/src/arraylift.ml
@@ -1,16 +1,5 @@
 open Syntax
 open Iteration
-module SMap = Syntax.Symbol.Map  
-
-let get_or_create_replacement srk replacement_map array_symbol index created_symbols isnt_forall = 
-  match Hashtbl.find_opt replacement_map index with 
-    | Some replacement -> mk_const srk replacement
-    | None -> (
-      let replacement = mk_symbol srk `TyInt ~name:("rep_" ^ (show_symbol srk array_symbol) ^ "_" ^ (ArithTerm.show srk index)) in
-      Hashtbl.add replacement_map index replacement;
-      (if (isnt_forall) then (created_symbols := replacement :: !created_symbols) else ()); (* Update the mutable list *)
-      mk_const srk replacement
-    )
 
 (* bubble_sym is an internal helper function for lowering formulas in the skolem array fragment
     to quantified numerical formulas. 
@@ -20,10 +9,21 @@ let get_or_create_replacement srk replacement_map array_symbol index created_sym
         - form' contains no array terms
 *) 
 let bubble_sym (srk : 'a context) (form : 'a formula) : (symbol list * (symbol option) * 'a formula) = 
-  (* rep_map : arr_var -> (index_var -> var) *)
+  (* rep_map : arr_var -> (index_term -> var) *)
   let (rep_map : (symbol, ('a arith_term, symbol) Hashtbl.t) Hashtbl.t) = Hashtbl.create 16 in 
   let created_symbols = ref [] in 
   let forall_symbol = mk_symbol srk `TyInt ~name:"forall_index" in
+
+  let get_or_create_replacement srk replacement_map array_symbol index_term isnt_forall = 
+    match Hashtbl.find_opt replacement_map index_term with 
+      | Some replacement -> mk_const srk replacement
+      | None -> (
+        let replacement = mk_symbol srk `TyInt ~name:("rep_" ^ (show_symbol srk array_symbol) ^ "_" ^ (ArithTerm.show srk index_term)) in
+        Hashtbl.add replacement_map index_term replacement;
+        (if (isnt_forall) then (created_symbols := replacement :: !created_symbols) else ()); (* Update the mutable list *)
+        mk_const srk replacement
+      )
+  in
 
   (* All variables are turned into symbols as we recurse across the formula.
     env maps from variable indices to symbols. 
@@ -34,26 +34,26 @@ let bubble_sym (srk : 'a context) (form : 'a formula) : (symbol list * (symbol o
   (* [replace_access arr i env qo] returns a substitute term for arr[i] *)
   let rec replace_access (arr : 'a arr_term) (index : 'a arith_term) (env : (symbol Env.t)) (qo : int) : 'a arith_term = 
     match ArrTerm.destruct srk arr with 
-    | `Var (i, _) -> if (i <= qo) then (failwith "Array term is not quantified inside the forall") else ( 
-      let array_symbol = Env.find env (i - qo) in 
-      let replacement_map = Hashtbl.find rep_map array_symbol in 
-      match ArithTerm.destruct srk index with 
-        | `Var (indi, _) -> 
-          ((if indi < qo then failwith "Array index variable is bound but should be free");
-          let index_symbol = Env.find env (indi - qo) in 
-          get_or_create_replacement srk replacement_map array_symbol (mk_const srk index_symbol) created_symbols (indi - qo != 0))
-        | `App (_, []) | `Real _ -> 
-          get_or_create_replacement srk replacement_map array_symbol index created_symbols true
-        | _ -> failwith "Array index should be a variable or a symbol"
-      ) 
+    | `Var (i, _) -> 
+        if (i <= qo) then (failwith "Array term is not quantified inside the forall") else ( 
+          let array_symbol = Env.find env (i - qo) in 
+          let replacement_map = Hashtbl.find rep_map array_symbol in 
+          match ArithTerm.destruct srk index with 
+            | `Var (indi, _) -> 
+              ((if indi < qo then failwith "Array index variable is bound but should be free");
+              let index_symbol = Env.find env (indi - qo) in 
+              get_or_create_replacement srk replacement_map array_symbol (mk_const srk index_symbol) (indi - qo != 0))
+            | `App (_, []) | `Real _ -> 
+              get_or_create_replacement srk replacement_map array_symbol index true
+            | _ -> failwith "Array index should be a variable, symbol, or constant"
+        ) 
     | `App (_, _) -> failwith "Array applications not supported"
     | `Ite (f, l, r) -> mk_ite srk (replace env qo f) (replace_access l index env qo) (replace_access r index env qo)
     | `Store (a, store_index, value) ->
       let store_index' = replace_at store_index env qo in 
       let index' = replace_at index env qo in 
-      let ae = replace_access a index env qo in 
-      let ret = mk_ite srk (mk_eq srk store_index' index') value ae in 
-      ret
+      let ae = replace_access a index' env qo in 
+      mk_ite srk (mk_eq srk store_index' index') value ae 
   (* replace_at recurses over arith_terms, performing substitution *)
   and replace_at (t : 'a arith_term) (env : (symbol Env.t)) (qo : int) : 'a arith_term = 
     match ArithTerm.destruct srk t with 
@@ -107,19 +107,16 @@ let bubble_sym (srk : 'a context) (form : 'a formula) : (symbol list * (symbol o
       let new_sym = mk_symbol srk ~name (typ :> typ) in 
       (if typ = `TyArr then ((Hashtbl.add rep_map new_sym (Hashtbl.create 16)))); 
       let (syms, forall, body) = go body (Env.push new_sym env) in
-      if typ = `TyArr then (
-        (syms , forall, body)
-      ) else (new_sym :: syms, forall, body) 
-      
+      if typ = `TyArr then (syms , forall, body) else (new_sym :: syms, forall, body) 
     | `Quantify (`Forall, _, typ, body) -> 
       assert (typ = `TyInt);
       let (body) = replace (Env.push forall_symbol env) 0 body in 
-      let fc = (Hashtbl.fold (fun array_symbol m (acc) -> 
-          let ai = get_or_create_replacement srk m array_symbol (mk_const srk forall_symbol) created_symbols false in 
+      let functional_correctness = (Hashtbl.fold (fun array_symbol m (acc) -> 
+          let ai = get_or_create_replacement srk m array_symbol (mk_const srk forall_symbol) false in 
           Hashtbl.fold (fun index replacement acc -> 
             (mk_if srk (mk_eq srk index (mk_const srk forall_symbol)) (mk_eq srk (mk_const srk replacement) ai)) :: acc) m acc
         ) rep_map ([])) in 
-      ([], true, mk_and srk (body :: fc))
+      ([], true, mk_and srk (body :: functional_correctness))
     | `And ls -> 
       let (syms, forall, parts) = List.fold_left (fun (syms, forall, parts) f -> 
         let (f_syms, f_forall, f_part) = go f env in 
@@ -179,7 +176,6 @@ let array_exponentiate (srk : 'a context) (e : 'a exp_op) : ('a TransitionFormul
       | Some s -> mk_forall_const srk s skol
     in
       
-      
     let with_everything = List.fold_left (fun acc s -> mk_exists_const srk s acc) with_universal existentials in 
 
     let eliminated = SrkZ3.qe srk with_everything in 
@@ -199,8 +195,8 @@ let array_exponentiate (srk : 'a context) (e : 'a exp_op) : ('a TransitionFormul
         if s = j' then mk_const srk j else mk_const srk s) ret in 
     
     mk_forall_const srk j ret 
-    in 
-    e'
+  in 
+  e'
 
 
   (* [map_elim ctx f] takes a formula in the array skolem fragment and returns an equisatisfiable formula over numerical variables. *)
