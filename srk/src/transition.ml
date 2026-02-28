7ee7e620cc3fc4aaf18c4d12b130490a6b35766e
Nicolas Koh <nclskh1@gmail.com>
2026-02-28 12:47:36 -0500

    Transition: Also do NNF rewriting when preprocessing formulas.

--- srk/src/transition.ml
+++ srk/src/transition.ml
@@ -506,6 +506,7 @@ struct
     |> Nonlinear.linearize srk
     |> Syntax.eliminate_floor_mod_div srk
     |> Syntax.eliminate_ite srk
+    |> rewrite srk ~down:(nnf_rewriter srk)
     |> rewrite srk ~down:(pos_rewriter srk)
     |> SrkApron.abstract ~exists srk man
 
